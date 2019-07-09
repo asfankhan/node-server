3c0140761a155d6b129d9a2426be35b59067a420
unknown <asfankhan0987@gmail.com>
2019-07-08 21:14:33 -0400

    Touble shooting it for global

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-var http = require('http');
-var express = require("express")
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(80, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:80/');
+// var http = require('http');
+// var express = require("express")
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World\n');
+// }).listen(80, '127.0.0.1');
+// console.log('Server running at http://127.0.0.1:80/');
+
+var express = require('express');
+var app = express();
+
+app.listen(3000, ()=>  {
+    console.log('listening');
+});
+
+app.get('/',(req,res)=>  {
+    res.send('hi');
+});
