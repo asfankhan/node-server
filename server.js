24a9ffb6b265b26e00663c66eecf5125b5e6221f
unknown <asfankhan0987@gmail.com>
2019-07-08 20:53:43 -0400

    added express

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var express = require("express")
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(80, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://127.0.0.1:80/');
