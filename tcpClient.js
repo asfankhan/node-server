5b7efe1c9b7981d6e6d02f493547d37e4d01c15e
asfankhan0987 <asfankhan0987@gmail.com>
2019-08-14 13:34:30 -0400

    commenting

--- tcpClient.js
+++ tcpClient.js
@@ -11,52 +11,50 @@ const awsHost = '54.237.223.142';
 // const client = new net.Socket();
 let server;
 
-let client = net.createConnection({host : awsHost, port : port}, () => {
 
-    console.log('> Connected to public server via local endpoint:', client.localAddress + ':' + client.localPort);
+let client = net.createConnection({host : host, port : port}, () => {
 
-    data = { address: client.localAddress, port: client.localPort, isServer: false };
-    console.log(data)
-    client.write(JSON.stringify(data))
-    c = require('net').createConnection({host : data.address, port : data.port},function () {
-        console.log('>(ClientB) connected to clientA!');
-    
-        c.on('data', function (data) {
-            console.log(data.toString());
+    console.log('> connected to public server via local endpoint:', client.localAddress + ':' + client.localPort);
+
+    if(process.env.SERVER == 'true'){
+        data = { address: client.localAddress, port: client.localPort, isServer: process.env.SERVER };
+        console.log(data)
+        client.write(JSON.stringify(data))
+    }else{
+        data = { address: client.localAddress, port: client.localPort, isServer: false };
+        console.log(data)
+        client.write(JSON.stringify(data))
+    }
+
+    // do not end the connection, keep it open to the public server
+    // and start a tcp server listening on the ip/port used to connected to server.js
+    if(process.env.SERVER == 'true'){
+        server = net.createServer( (socket) => {
+            console.log('> (clientA) someone connected, it\s:', socket.remoteAddress, socket.remotePort);
+            socket.write("Hello there NAT traversal man, this is a message from a client behind a NAT!");
+        }).listen(client.localPort, client.localAddress, function (err) {
+            if(err) return console.log(err);
+            console.log('> (clientA) listening on:', client.localAddress + ':' + client.localPort);
         });
-        c.on('close', function() {
-            console.log('Connection closed');
-        });
-        
-        c.on("error", (err) =>{
-            console.log("Caught flash policy server socket error: ")
-            console.log(err.stack)
-        });
-    });
+    }else{
+
+    }
+
 });
 
 var c;
-client.on('data', function(data) {
-    console.log(">(ClientB) " + data.toString());
-
+this.serverData = {}
+client.on('data', (data) => {
+    console.log(">(Client) " + data.toString());
     data = JSON.parse(data)
-
-    c = require('net').createConnection({host : data.address, port : data.port},function () {
-        console.log('>(ClientB) connected to clientA!');
+    this.serverData = data
+    // c = require('net').createConnection({host : data.address, port : data.port},function () {
+    //     console.log('> (clientB) connected to clientA!');
     
-        c.on('data', function (data) {
-            console.log(data.toString());
-        });
-        c.on('close', function() {
-            console.log('Connection closed');
-        });
-        
-        c.on("error", (err) =>{
-            console.log("Caught flash policy server socket error: ")
-            console.log(err.stack)
-        });
-    });
-
+    //     c.on('data', function (data) {
+    //         console.log(data.toString());
+    //     });
+    // });
 });
 
 client.on('close', function() {
@@ -81,58 +79,22 @@ rl.on('line', (input) => {
         rl.close();
     
     console.log("Input: " +input);
-    // client.write( input + " " + client.address().address);    
-    c.write( input + "- Client:" + client.address().address+":"+client.address().port);    
+    // client.write( input + " " + client.address().address);  
+    console.log("this.serverData: ", this.serverData);
+  
+    if(c){
+        c.write( input + "- Client:" + client.address().address+":"+client.address().port);    
+    }else{
+        c = require('net').createConnection({host : this.serverData.address, port : this.serverData.port},function () {
+            console.log('> (clientB) connected to clientA!');
+        
+            c.on('data', function (data) {
+                console.log(data.toString());
+            });
+        });
+    }
 
 }).on('close', () => {
     console.log('Program Ended');
     process.exit(0);
 });
-
-// var path = require('path');
-// const express = require('express'),
-//     bodyParser = require('body-parser');
-
-// const app = express();
-
-// app.use(express.static(__dirname + '/client'));
-// app.use(express.json());
-
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname + './client/client.html'));
-// });
-
-// app.post('/test', function(request, response) {
-//     console.log(request.body);
-//     let data = request.body
-//     console.log(data.address);
-//     console.log(data.port);
-
-//     net.createConnection({host : awsHost, port : port}, () => {
-
-//         console.log('> Connected to public server via local endpoint:', client.localAddress + ':' + client.localPort);
-    
-//         data = { address: client.localAddress, port: client.localPort, isServer: false };
-//         console.log(data)
-//         client.write(JSON.stringify(data))
-//         c = require('net').createConnection({host : data.address, port : data.port},function () {
-//             console.log('>(ClientB) connected to clientA!');
-        
-//             c.on('data', function (data) {
-//                 console.log(data.toString());
-//             });
-//             c.on('close', function() {
-//                 console.log('Connection closed');
-//             });
-            
-//             c.on("error", (err) =>{
-//                 console.log("Caught flash policy server socket error: ")
-//                 console.log(err.stack)
-//             });
-//         });
-//     });
-
-
-//  });
-
-// app.listen(8082);
--- tcpServer.js
+++ tcpServer.js
@@ -72,23 +72,23 @@ server.on('connection', (socket) => {
 });
 
 
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname + './public/index.html'));
-// });
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + './public/index.html'));
+});
 
-// app.get('/test', function(req, res, next) {
-//     res.json(serverSockets);
-// });
+app.get('/test', function(req, res, next) {
+    res.json(serverSockets);
+});
 
 
-// app.listen(8080, host, () => {
-//     console.log('>(Server) Website on : 8000');
-// });
+app.listen(8080, host, () => {
+    console.log('>(Server) Website on : 8000');
+});
